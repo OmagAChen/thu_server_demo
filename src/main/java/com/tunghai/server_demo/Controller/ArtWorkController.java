9724e9acc51256457a9af4e5dcce3676418444ec
OmagA <zaqwsxcdsa@gmail.com>
2021-11-14 16:19:19 +0800

    將有關NLPI_Controller對於這次展覽的作品導向移除

--- src/main/java/com/tunghai/server_demo/Controller/ArtWorkController.java
+++ src/main/java/com/tunghai/server_demo/Controller/ArtWorkController.java
@@ -15,18 +15,6 @@ public class ArtWorkController {
     @Autowired    //注入Service的依賴
     ArtWorkService artWorkService;
 
-    @RequestMapping("/AI_Art_Tech_MainPage")
-    public String aI_Art_Tech_MainPage() {
-        return "AI_Art_Tech_MainPage";
-    }
-
-    @RequestMapping("/Show_No.2_ArtWorks")
-    public String Show_No2_ArtWorks(Model model) {
-        Iterable<ArtWorkModel> artWorkList = artWorkService.getArtWorkModels();
-        model.addAttribute("artWorkList", artWorkList);
-        return "Show_No2_ArtWorks";
-    }
-
     @GetMapping("/showArtWork")
     public String showArtWork(@RequestParam(value="filePath") String filePath, Model model) {
         ArtWorkModel artWorkModel = artWorkService.getArtWorkModelByFilePath(filePath);
@@ -64,13 +52,14 @@ public class ArtWorkController {
         return "editArtWork";
     }
     @PostMapping("/editArtWork")
+    @ResponseBody
     public String createArtWork(@RequestParam("file") MultipartFile file, @ModelAttribute ArtWorkModel artWorkModel, Model model) {
         Iterable<ArtWorkModel> allArtWorkList = artWorkService.createArtWorkModel(file,artWorkModel);
         //回傳更新過後的作品列表
         model.addAttribute("artWorkList", allArtWorkList);
         ArtWorkModel emptyArtWorkModel = new ArtWorkModel();
         model.addAttribute("artWorkObject", emptyArtWorkModel);
-        return "redirect:/editArtWork";
+        return artWorkModel.getFilePath();
     }
 
 }
